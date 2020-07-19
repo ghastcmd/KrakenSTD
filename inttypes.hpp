3ba78d4703766ff33dbe3813253c04eb17be2b39
Kraken32bin <kraken32bin@protonmail.com>
2020-07-19 02:20:33 -0300

    Made size_t a standalone type; Created a standard true type

--- inttypes.hpp
+++ inttypes.hpp
@@ -6,12 +6,12 @@ using uint16_t = unsigned short;
 using uint8_t  = unsigned char;
 
 using int64_t = signed long long;
-using int32_t = int;
+using int32_t = signed int;
 using int16_t = signed short;
 using int8_t  = signed char;
 
-using size_t = uint64_t;
-using byte = uint8_t;
+using size_t = long unsigned int;
+using byte   = unsigned char;
 
 template <typename _ty>
 struct sizes {};
@@ -48,7 +48,13 @@ struct sizes <uint64_t> {
     static constexpr byte len = 20;
 };
 
-
+template <>
+struct sizes <size_t> {
+    using type = size_t;
+    static constexpr type max = 0xffffffffffffffff;
+    static constexpr type min = 0x0000000000000000;
+    static constexpr byte len = 20;
+};
 
 
 template <>
@@ -84,65 +90,81 @@ struct sizes <int64_t> {
 };
 
 
-
-template <typename _ty>
-constexpr bool is_integer()
-{ return false; }
+template <>
+struct sizes <float> {
+    using type = float;
+    static constexpr type len = 15;
+};
 
 template <>
-constexpr bool is_integer <int8_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <int16_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <int32_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <int64_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <uint8_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <uint16_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <uint32_t>()
-{ return true; }
-
-template <>
-constexpr bool is_integer <uint64_t>()
-{ return true; }
-
-
+struct sizes <double> {
+    using type = double;
+    static constexpr type len = 24;
+};
 
 
 template <typename _ty>
-constexpr bool is_unsigned()
-{ return false; }
+constexpr byte hex_len = sizeof(sizes<_ty>::max) * 2 + 2;
+
+
+template <typename _ty, _ty val>
+struct lit_val
+{ static constexpr _ty value = val; };
+
+using true_type  = lit_val<bool, true>;
+using false_type = lit_val<bool, false>;
+
+
+template <typename _ty>
+struct is_integer : false_type {};
 
 template <>
-constexpr bool is_unsigned <uint8_t>()
-{ return true; }
+struct is_integer <int8_t>   : true_type {};
 
 template <>
-constexpr bool is_unsigned <uint16_t>()
-{ return true; }
+struct is_integer <int16_t>  : true_type {};
 
 template <>
-constexpr bool is_unsigned <uint32_t>()
-{ return true; }
+struct is_integer <int32_t>  : true_type {};
 
 template <>
-constexpr bool is_unsigned <uint64_t>()
-{ return true; }
+struct is_integer <int64_t>  : true_type {};
+
+template <>
+struct is_integer <uint8_t>  : true_type {};
+
+template <>
+struct is_integer <uint16_t> : true_type {};
+
+template <>
+struct is_integer <uint32_t> : true_type {};
+
+template <>
+struct is_integer <uint64_t> : true_type {};
+
+template <>
+struct is_integer <size_t>   : true_type {};
+
+template <typename _ty>
+using is_integer_v = typename is_integer<_ty>::value;
+
+
+
+template <typename _ty>
+struct is_unsigned : false_type {};
+
+template <>
+struct is_unsigned <uint8_t>  : true_type {};
+
+template <>
+struct is_unsigned <uint16_t> : true_type {};
+
+template <>
+struct is_unsigned <uint32_t> : true_type {};
+
+template <>
+struct is_unsigned <uint64_t> : true_type {};
+
 
 
 template <bool _test, typename _ty = void>
@@ -158,3 +180,13 @@ struct enable_if<false, _ty> {};
 
 template <bool _test, typename _ty = void>
 using enable_if_t = typename enable_if<_test, _ty>::type;
+
+
+
+template <typename _ty1, typename _ty2>
+struct is_same
+{ static constexpr bool value = false; };
+
+template <typename _ty>
+struct is_same<_ty, _ty>
+{ static constexpr bool value = true; };
